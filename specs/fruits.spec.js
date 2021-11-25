0714076d2a8a27362579d5777d6e1615802e54ce
popadicbranislav <popadicbranislav@gmail.com>
2021-11-25 21:37:14 +0100

    testing vuex store

--- specs/fruits.spec.js
+++ specs/fruits.spec.js
@@ -1,2 +1,23 @@
-import  FruitBasket from '@/fruit-basket'
-import { mount } from '@vue/test-utils'
+import FruitBasket from "@/fruit-basket";
+import { mount } from "@vue/test-utils";
+
+test("can add fruits to the basket with DOM", () => {
+  const wrapper = mount(FruitBasket);
+
+  const input = wrapper.find("input");
+  const button = wrapper.find("button");
+
+  expect(wrapper.findAll("li").length).toBe(0);
+
+  input.element.value = "banana";
+  input.trigger("input");
+  expect(wrapper.vm.fruit).toBe("banana");
+
+  button.trigger("click");
+
+  expect(wrapper.vm.fruit).toBe("");
+  expect(wrapper.vm.basket).toEqual(expect.arrayContaining(["banana"]));
+  wrapper.vm.$nextTick(() => {
+    expect(wrapper.findAll("li").length).toBe(1);
+  });
+});
